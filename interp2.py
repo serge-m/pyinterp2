7d65306a7a57c6b69ff8df832a209ef16bf5274f
Sergey Matyunin <matyunin.vg@gmail.com>
2014-09-16 01:56:47 +0400

    Refactored according to PEP

--- interp2.py
+++ interp2.py
@@ -2,17 +2,11 @@ __author__ = 'Sergey Matyunin'
 
 import numpy as np
 
-# z = I1_
-# s = np.array([0.5])
-# t = np.array([0.5])
-#z,XI,YI = I1_, idx,idy
-#------------------------------------------------------
-def interp2linear(z, XI, YI, extrapval=np.nan):
-# if 1:
-    x = XI.copy()
-    y = YI.copy()
-    nrows, ncols = z.shape;
-    #     print "nrows,ncols", nrows,ncols
+
+def interp2linear(z, xi, yi, extrapval=np.nan):
+    x = xi.copy()
+    y = yi.copy()
+    nrows, ncols = z.shape
 
     if nrows < 2 or ncols < 2:
         raise Exception("z shape is too small")
@@ -22,67 +16,44 @@ def interp2linear(z, XI, YI, extrapval=np.nan):
 
 
     # find x values out of range
-    x_bad = ( (x < 0) | (x > ncols - 1));
-    #     print "x_bad", x_bad
+    x_bad = ( (x < 0) | (x > ncols - 1))
     if x_bad.any():
-    #         print "any!"
         x[x_bad] = 0
 
     # find y values out of range
-    y_bad = ((y < 0) | (y > nrows - 1));
-    #     print "y_bad", y_bad
-
+    y_bad = ((y < 0) | (y > nrows - 1))
     if y_bad.any():
-    #         print "any!"
         y[y_bad] = 0
 
     # linear indexing. z must be in 'C' order
-    ndx = np.floor(y) * ncols + np.floor(x);
+    ndx = np.floor(y) * ncols + np.floor(x)
     ndx = ndx.astype('int32')
-    #     print "ndx", ndx
 
     # fix parameters on x border
-    #     if x.empty():
-    #         d = x;
-    #     else:
-    #     print "x", x
-    #     print "ncols-1", ncols-1
-    d = (x == ncols - 1);
-    x = (x - np.floor(x));
+    d = (x == ncols - 1)
+    x = (x - np.floor(x))
     if d.any():
-        x[d] += 1;
-        ndx[d] -= 1;
-        #     print "ndx x", ndx
+        x[d] += 1
+        ndx[d] -= 1
 
     # fix parameters on y border
     d = (y == nrows - 1)
-    y = (y - np.floor(y));
+    y = (y - np.floor(y))
     if d.any():
-        y[d] += 1;
-        ndx[d] -= ncols;
-        #     print "ndx y", ndx
+        y[d] += 1
+        ndx[d] -= ncols
 
     # interpolate
-    one_minus_t = 1 - y;
+    one_minus_t = 1 - y
     z = z.ravel()
-    #     print "ndx", ndx
-    #x, y = y, x
-    #print "z[ndx]", z[ndx]
-    #print "x", x
-    #print "z[ndx]*(one_minus_t)", z[ndx]*(one_minus_t)
-    #     print "z[ndx+1]*y", z[ndx+1]*y
-    #     print "z[ndx+ncols]", z[ndx+ncols]
-    #     print "ndx+(ncols)+1", ndx+(ncols)+1
-    #     print "z[ndx+(ncols)+1]", z[ndx+(ncols)+1]
-    F = ( z[ndx] * (one_minus_t) + z[ndx + ncols] * y ) * (1 - x) + ( z[ndx + 1] * (one_minus_t) + z[ndx + (ncols) + 1] * y ) * x;
+    f = (z[ndx] * one_minus_t + z[ndx + ncols] * y ) * (1 - x) + (
+        z[ndx + 1] * one_minus_t + z[ndx + ncols + 1] * y) * x
 
     # Set out of range positions to extrapval
     if x_bad.any():
-        F[x_bad] = extrapval;
+        f[x_bad] = extrapval
     if y_bad.any():
-        F[y_bad] = extrapval;
+        f[y_bad] = extrapval
 
+    return f
 
-    return F
-    # print F
-    # z[ndx], z[ndx+1], z[ndx+ncols]
--- test/test_interp2.py
+++ test/test_interp2.py
@@ -1,4 +1,4 @@
-__author__ = 's'
+__author__ = 'Sergey Matyunin'
 
 import unittest
 import numpy as np
@@ -8,38 +8,33 @@ from StringIO import StringIO
 
 
 class MyTestCase(unittest.TestCase):
-    #def test_something(self):
-    #    self.assertEqual(True, False)
-
-
     def test_interp2linear(self):
-        I1 = np.array([
+        i1 = np.array([
             [0.5, 0., 0.],
             [1., 0., 0.],
             [0.5, 0., 0.],
             [0., 1., 0.],
         ])
 
-        idx, idy = np.meshgrid(np.arange(I1.shape[1]), np.arange(I1.shape[0]))
+        idx, idy = np.meshgrid(np.arange(i1.shape[1]), np.arange(i1.shape[0]))
 
-        self.assertTrue(np.array_equal(interp2linear(I1, idx, idy, ), I1))
-        self.assertTrue(np.array_equal(interp2linear(I1, idx.ravel(), idy.ravel(), ), I1.ravel()))
+        self.assertTrue(np.array_equal(interp2linear(i1, idx, idy, ), i1))
+        self.assertTrue(np.array_equal(interp2linear(i1, idx.ravel(), idy.ravel(), ), i1.ravel()))
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, idx+0.5, idy, ),
+            interp2linear(i1, idx + 0.5, idy, ),
             np.array(
-                [[ 0.25,  0.,     nan],
-                 [ 0.5 ,  0.,     nan],
-                 [ 0.25,  0.,     nan],
-                 [ 0.5 ,  0.5,    nan]]
+                [[0.25, 0., nan],
+                 [0.5, 0., nan],
+                 [0.25, 0., nan],
+                 [0.5, 0.5, nan]]
             ),
             atol=1e-10,
             equal_nan=True,
         ).all())
 
-
         self.assertTrue(np.isclose(
-            interp2linear(I1, idx, idy+0.5, ),
+            interp2linear(i1, idx, idy + 0.5, ),
             np.loadtxt(StringIO("""
                 0.7500         0         0
                 0.7500         0         0
@@ -51,7 +46,7 @@ class MyTestCase(unittest.TestCase):
         ).all())
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, idx+0.5, idy+0.5, ),
+            interp2linear(i1, idx + 0.5, idy + 0.5, ),
             np.loadtxt(StringIO("""
                 0.3750         0       NaN
                 0.3750         0       NaN
@@ -63,7 +58,7 @@ class MyTestCase(unittest.TestCase):
         ).all())
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, idx-1.5, idy, ),
+            interp2linear(i1, idx - 1.5, idy, ),
             np.loadtxt(StringIO("""
                 NaN       NaN    0.2500
                 NaN       NaN    0.5000
@@ -75,7 +70,7 @@ class MyTestCase(unittest.TestCase):
         ).all())
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, idx-1.5, idy-1.5, ),
+            interp2linear(i1, idx - 1.5, idy - 1.5, ),
             np.loadtxt(StringIO("""
                 NaN       NaN       NaN
                 NaN       NaN       NaN
@@ -87,7 +82,7 @@ class MyTestCase(unittest.TestCase):
         ).all())
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, idx, idy-1.5, extrapval=99),
+            interp2linear(i1, idx, idy - 1.5, extrapval=99),
             np.loadtxt(StringIO("""
                 99       99       99
                 99       99       99
@@ -99,7 +94,7 @@ class MyTestCase(unittest.TestCase):
         ).all())
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, np.array([1000.3, 1000.4]).T, np.array([1000.5, -1000.6]).T),
+            interp2linear(i1, np.array([1000.3, 1000.4]).T, np.array([1000.5, -1000.6]).T),
             np.loadtxt(StringIO("""
                 nan
                 nan"""
@@ -109,13 +104,12 @@ class MyTestCase(unittest.TestCase):
         ).all())
 
         self.assertTrue(np.isclose(
-            interp2linear(I1, np.array([0.3, 1.4]), np.array([0.5, 0.6]), extrapval=99),
+            interp2linear(i1, np.array([0.3, 1.4]), np.array([0.5, 0.6]), extrapval=99),
             np.loadtxt(StringIO("""0.5250         0""")),
             atol=1e-10,
             equal_nan=True,
         ).all())
         pass
-
 
 
 if __name__ == '__main__':
